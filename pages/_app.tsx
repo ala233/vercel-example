a6adfeda7169f6f68373ef560e1a476b254001f2
ala233 <lyzstella527@gmail.com>
2023-09-25 17:35:47 +1000

    Step 6. Set up GitHub authentication with NextAuth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
-import { AppProps } from "next/app";
+import { Session } from "next-auth";
+import { SessionProvider } from 'next-auth/react';
+import { AppProps } from 'next/app';
 
-const App = ({ Component, pageProps }: AppProps) => {
+function App({
+  Component,
+  pageProps,
+}: AppProps<{
+  session: Session;
+}>) {
+  
   return (
-    <Component {...pageProps} />
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
   );
-};
+}
 
 export default App;
