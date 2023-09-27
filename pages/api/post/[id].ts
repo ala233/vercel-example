7f1dbcbff6bc8b0f467bdba1852f1ea653f8eb47
ala233 <lyzstella527@gmail.com>
2023-09-27 11:48:41 +1000

    Step 11. Add Delete functionality

--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -1,15 +1,12 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../lib/prisma';
 
 // DELETE /api/post/:id
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const postId = typeof (req.query.id) !== 'string' ? req.query.id[0] : req.query.id;
-
+export default async function handle(req, res) {
+  const postId = req.query.id;
   if (req.method === 'DELETE') {
     const post = await prisma.post.delete({
       where: { id: postId },
     });
-    
     res.json(post);
   } else {
     throw new Error(
@@ -17,5 +14,3 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     );
   }
 }
-
-export default handler;
--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -18,23 +18,57 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
       },
     },
   });
+
   return {
     props: post,
   };
 };
 
-async function publishPost(id: string): Promise<void> {
+const publishPost = async (id: string): Promise<void> => {
   await fetch(`/api/publish/${id}`, {
     method: 'PUT',
   });
+
+  await Router.push('/');
+}
+
+const deletePost = async (id: string): Promise<void> => {
+  await fetch(`/api/post/${id}`, {
+    method: 'DELETE'
+  });
+
   await Router.push('/');
 }
 
 const Post: React.FC<PostProps> = (props) => {
+  const css = `
+    .page {
+      background: var(--geist-background);
+      padding: 2rem;
+    }
+
+    .actions {
+      margin-top: 2rem;
+    }
+
+    button {
+      background: #ececec;
+      border: 0;
+      border-radius: 0.125rem;
+      padding: 1rem 2rem;
+    }
+
+    button + button {
+      margin-left: 1rem;
+    }
+  `;
+
   const { data: session, status } = useSession();
+
   if (status === 'loading') {
     return <div>Authenticating ...</div>;
   }
+
   const userHasValidSession = Boolean(session);
   const postBelongsToUser = session?.user?.email === props.author?.email;
   let title = props.title;
@@ -48,9 +82,16 @@ const Post: React.FC<PostProps> = (props) => {
         <h2>{title}</h2>
         <p>By {props?.author?.name || 'Unknown author'}</p>
         <ReactMarkdown children={props.content} />
-        {!props.published && userHasValidSession && postBelongsToUser && (
-          <button onClick={() => publishPost(props.id)}>Publish</button>
-        )}
+        {
+          !props.published && userHasValidSession && postBelongsToUser && (
+            <button onClick={() => publishPost(props.id)}>Publish</button>
+          )
+        }
+        {
+          userHasValidSession && postBelongsToUser && (
+            <button onClick={() => deletePost(props.id)}>Delete</button>
+          )
+        }
       </div>
       <style jsx>{`
         .page {
